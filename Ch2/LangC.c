ea4f8dcf2fa06ab6ba07e5b0bb3d96352af276ab
조성찬 <sungchan070613@gmail.com>
2026-09-08 17:16:15 +0900

    Add files via upload

---
+++ Ch2/LangC.c
@@ -0,0 +1,18 @@
+//
+
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+int main(void) {
+	/* int num = 10; //char: 1바이트 int: 4바이트 
+	int* p = &num; // &num <- num의 주소
+	printf("%d\n", num); //10 출력 - 직접참조
+	//변수명 접근(직접 참조)
+	//주소로 접근(간접 참조) - 포인터
+	printf("%d\n", *p); //10 출력 - 간접참조  // 10 -> 4번지부터 4바이트만큼 읽음
+	//printf("%d\n", *&num); *(&num) -> *(주소) => 10
+	*p = 20; //p가 가리키는 공간에 20을 대입  // 20 -> 4번지부터 4바이트만큼 읽음
+	printf("%d\n", *p); 
+	return 0; */
+}
